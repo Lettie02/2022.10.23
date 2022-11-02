897dc45b3a21fb0a8fca3f099252e1690988d377
Lettie02 <nahmanyesther1@gmail.com>
2022-11-02 12:11:30 +0200

    final

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const students = [
-  { name: "Ray", course: "Computer Science", isPassed: true },
-  { name: "Liam", course: "Computer Science", isPassed: false },
-  { name: "Jenner", course: "Information Technology", isPassed: true },
-  { name: "Marco", course: "Robotics", isPassed: true },
-  { name: "Kimberly", course: "Artificial Intelligence", isPassed: false },
-  { name: "Jamie", course: "Big Data", isPassed: false },
-];
-let pass = students.filter((student) => student.isPassed === true);
-console.log(pass);
+// const students = [
+//   { name: "Ray", course: "Computer Science", isPassed: true },
+//   { name: "Liam", course: "Computer Science", isPassed: false },
+//   { name: "Jenner", course: "Information Technology", isPassed: true },
+//   { name: "Marco", course: "Robotics", isPassed: true },
+//   { name: "Kimberly", course: "Artificial Intelligence", isPassed: false },
+//   { name: "Jamie", course: "Big Data", isPassed: false },
+// ];
+// let passedStudents = students.filter((student) => student.isPassed === true);
+// console.log(passedStudents);
+
+// let number = Math.floor(Math.random() * 100);
+// const compareToTen = (num) => {
+//   return new Promise((resolve, reject) => {
+//     if (num > 10) {
+//       resolve(num);
+//     } else if (num > 10) {
+//       reject(num);
+//     }
+//   });
+// };
